6702f5a3f18f7000f78a3aa9179de58c2705727b
s25866 <s25866@pjwstk.edu.pl>
2022-10-26 16:27:24 +0200

    polecenie 2

--- src/main/java/org/example/model/Order.java
+++ src/main/java/org/example/model/Order.java
@@ -1,11 +1,15 @@
 package org.example.model;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@AllArgsConstructor
 public class Order {
-    public int numer;
-    public Pizza pizza;
-    public Order(int numer,Pizza pizza){
-        this.numer=numer;
-        this.pizza=pizza;
+    @Getter @Setter public int numer;
+    @Getter @Setter public Pizza pizza;
 
-    }
+
+    //    public Order(int numer,Pizza pizza){
+//        this.numer=numer;
+//        this.pizza=pizza;
+//    }
 }
--- src/main/java/org/example/model/Pizza.java
+++ src/main/java/org/example/model/Pizza.java
@@ -1,11 +1,17 @@
 package org.example.model;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Pizza {
-    public String name;
-    public int cena;
-    public Pizza(String name,int cena){
-        this.cena=cena;
-        this.name=name;
-    }
+    @Getter @Setter public String name;
+    @Getter @Setter public int cena;
+//    public Pizza(String name,int cena){
+//        this.cena=cena;
+//        this.name=name;
+//    }
 
 }
